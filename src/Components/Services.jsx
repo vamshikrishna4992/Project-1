807efa6b3a82090714da3403246106e7b8a88fae
prakashdoppalapudi <147047895+prakashdoppalapudi@users.noreply.github.com>
2025-02-07 14:58:27 +0530

    some changes made

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+
 import "../Styles/Services.css";
 import { Link } from "react-router-dom";
+import Service1 from '../assets/images/Cloud Services.gif'
+import Service2 from '../assets/images/Digital Marketing.gif'
+import Service3 from '../assets/images/IT Consulting.gif'
+import Serivce4 from '../assets/images/Web Development.gif'
 
 
 
@@ -20,23 +24,23 @@ const Services = () => {
  
       <div className="services-card">
     <div className="services-rows">
-      <img src="service1.gif" alt=""/>
-      <h5>Software Development</h5>
+      <img src= {Serivce4} alt=""/>
+      <p>Software Development</p>
       <Link to="/software-development"><button className="hover-button">View</button></Link>
     </div>
     <div className="services-rows">
-      <img src="service2.gif" alt=""/>
-      <h5>Cloud Services</h5>
+      <img src= {Service1} alt=""/>
+      <p>Cloud Services</p>
       <Link to="/cloud-services"><button className="hover-button">View</button></Link>
     </div>
     <div className="services-rows">
-      <img src="service3.gif" alt=""/>
-      <h5>IT Staffing</h5>
+      <img src= {Service3} alt=""/>
+      <p>IT Staffing</p>
        <Link to="/it-staffing"><button className="hover-button">View</button></Link>
     </div>
     <div className="services-rows">
-      <img src="project4.gif" alt=""/>
-      <h5>Digital Marketing</h5>
+      <img src= {Service2} alt=""/>
+      <p>Digital Marketing</p>
       <Link to="/digital-marketing"><button className="hover-button">View</button></Link>
     </div>
   </div>

4260ae0f5b4ffd4899c6f45d7847a0d3f914a70b
aravindreddy144 <korepu.aravindreddy144@gmail.com>
2025-02-11 17:36:09 +0530

    Responsiveness issues are fixed: AK

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../Styles/Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa"; // Icons for mobile menu
+import { FaBars, FaTimes } from "react-icons/fa"; 
 
 const Navbar = () => {
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
@@ -11,18 +11,15 @@ const Navbar = () => {
   const handleNavigation = (path) => {
     navigate(path);
     setIsSubmenuOpen(false);
-    setIsMobileMenuOpen(false); // Close mobile menu on navigation
+    setIsMobileMenuOpen(false); 
   };
 
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/" onClick={() => setIsMobileMenuOpen(false)}>
-          <img src="./KN.png" alt="logo" />
-        </Link>
-        <Link to="/" onClick={() => setIsMobileMenuOpen(false)}>
-          <h3>KrystalNest</h3>
-        </Link>
+       <Link to='/' onClick={() => setIsMobileMenuOpen(false)}> <img src="./KN.png" alt="logo" /></Link>
+       <Link to='/' onClick={() => setIsMobileMenuOpen(false)}> <h3>KrystalNest</h3></Link>
+      
       </div>
 
       {/* Hamburger Menu (for mobile) */}
@@ -32,46 +29,26 @@ const Navbar = () => {
 
       {/* Navigation Links */}
       <div className={`all-nav-links ${isMobileMenuOpen ? "mobile-active" : ""}`}>
-        <ul className="nav-links">
-          <li>
-            <Link to="/" onClick={() => setIsMobileMenuOpen(false)}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" onClick={() => setIsMobileMenuOpen(false)}>
-              About
-            </Link>
-          </li>
-          <li
-            className="services"
-            onMouseEnter={() => setIsSubmenuOpen(true)}
-            onMouseLeave={() => setIsSubmenuOpen(false)}
-          >
-            <Link to="#" onClick={() => setIsMobileMenuOpen(false)}>
-              Services
-            </Link>
-            {isSubmenuOpen && (
-              <ul className="submenu">
-                <li onClick={() => handleNavigation("/software-development")}>Software Development</li>
-                <li onClick={() => handleNavigation("/it-staffing")}>IT Staffing</li>
-                <li onClick={() => handleNavigation("/cloud-services")}>Cloud Services</li>
-                <li onClick={() => handleNavigation("/digital-marketing")}>Digital Marketing</li>
+              <ul className="nav-links">
+                <li><Link to='/' onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
+                <li><Link to='/about' onClick={() => setIsMobileMenuOpen(false)}>About</Link></li>
+                <li className="services" 
+                    onMouseEnter={() => setIsSubmenuOpen(true)}
+                    onMouseLeave={() => setIsSubmenuOpen(false)}>
+                  <Link to='#' onClick={() => setIsMobileMenuOpen(false)}>Services</Link>
+                  {isSubmenuOpen && (
+                    <ul className="submenu">
+                      <li onClick={() => handleNavigation('/software-development')}>Software Development</li>
+                      <li onClick={() => handleNavigation('/it-staffing')}>IT Staffing</li>
+                      <li onClick={() => handleNavigation('/cloud-services')}>Cloud Services</li>
+                      <li onClick={() => handleNavigation('/digital-marketing')}>Digital Marketing</li>
+                    </ul>
+                  )}
+                </li>
+                <li><Link to='/project1' onClick={() => setIsMobileMenuOpen(false)}>Projects</Link></li>
               </ul>
-            )}
-          </li>
-          <li>
-            <Link to="/project1" onClick={() => setIsMobileMenuOpen(false)}>
-              Projects
-            </Link>
-          </li>
-        </ul>
         <ul>
-          <li className="nav-contact-btn">
-            <Link to="/contact" className="contactus" onClick={() => setIsMobileMenuOpen(false)}>
-              Contact Us
-            </Link>
-          </li>
+          <li className="nav-contact-btn"><Link to='/contact' className="contactus" onClick={() => setIsMobileMenuOpen(false)}>Contact Us</Link></li>
         </ul>
       </div>
     </nav>
